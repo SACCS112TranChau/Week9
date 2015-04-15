7229b039ecc675a75f0cf9636adf97cd63672800
A208 <A208@ISA2080011.sac.rsccd.inst>
2015-04-14 20:26:43 -0700

    Week 9 Lab 1

---
+++ Lab1GuessANumber/src/GuessANumber.java
@@ -0,0 +1,10 @@
+
+public class GuessANumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		RandomNumber rNumber = new RandomNumber();
+		System.out.print("The random number is: "+  rNumber.RanNumber());
+	}
+
+}
---
+++ Lab1GuessANumber/src/RandomNumber.java
@@ -0,0 +1,10 @@
+
+public class RandomNumber {
+	private int ranNumber = 0 + (int) ( Math.random() * 10 );
+	
+	public int RanNumber()
+	{
+		return ranNumber;
+	}
+
+}
